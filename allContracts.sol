4ee54f2e9add9bd390a47c94a050c7a969db99a2
Nicolas Cantu <nicolas.cantu@00smartvillage.com>
2017-06-07 15:22:02 +0200

    Update allContracts.sol
    add a bid collector

--- allContracts.sol
+++ allContracts.sol
@@ -83,7 +83,101 @@ contract Influence  {
     }
 }
 
-contract Coin is Wallet, Influence {
+contract tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData); }
+
+contract MyToken {
+    /* Public variables of the token */
+    string public standard = 'Token 0.1';
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    uint256 public totalSupply;
+
+    /* This creates an array with all balances */
+    mapping (address => uint256) public balanceOf;
+    mapping (address => mapping (address => uint256)) public allowance;
+
+    /* This generates a public event on the blockchain that will notify clients */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /* This notifies clients about the amount burnt */
+    event Burn(address indexed from, uint256 value);
+
+    /* Initializes contract with initial supply tokens to the creator of the contract */
+    function MyToken(
+        uint256 initialSupply,
+        string tokenName,
+        uint8 decimalUnits,
+        string tokenSymbol
+        ) {
+        balanceOf[msg.sender] = initialSupply;              // Give the creator all initial tokens
+        totalSupply = initialSupply;                        // Update total supply
+        name = tokenName;                                   // Set the name for display purposes
+        symbol = tokenSymbol;                               // Set the symbol for display purposes
+        decimals = decimalUnits;                            // Amount of decimals for display purposes
+    }
+
+    /* Send coins */
+    function transfer(address _to, uint256 _value) {
+        if (_to == 0x0) throw;                               // Prevent transfer to 0x0 address. Use burn() instead
+        if (balanceOf[msg.sender] < _value) throw;           // Check if the sender has enough
+        if (balanceOf[_to] + _value < balanceOf[_to]) throw; // Check for overflows
+        balanceOf[msg.sender] -= _value;                     // Subtract from the sender
+        balanceOf[_to] += _value;                            // Add the same to the recipient
+        Transfer(msg.sender, _to, _value);                   // Notify anyone listening that this transfer took place
+    }
+
+    /* Allow another contract to spend some tokens in your behalf */
+    function approve(address _spender, uint256 _value)
+        returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        return true;
+    }
+
+    /* Approve and then communicate the approved contract in a single tx */
+    function approveAndCall(address _spender, uint256 _value, bytes _extraData)
+        returns (bool success) {
+        tokenRecipient spender = tokenRecipient(_spender);
+        if (approve(_spender, _value)) {
+            spender.receiveApproval(msg.sender, _value, this, _extraData);
+            return true;
+        }
+    }        
+
+    /* A contract attempts to get the coins */
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
+        if (_to == 0x0) throw;                                // Prevent transfer to 0x0 address. Use burn() instead
+        if (balanceOf[_from] < _value) throw;                 // Check if the sender has enough
+        if (balanceOf[_to] + _value < balanceOf[_to]) throw;  // Check for overflows
+        if (_value > allowance[_from][msg.sender]) throw;     // Check allowance
+        balanceOf[_from] -= _value;                           // Subtract from the sender
+        balanceOf[_to] += _value;                             // Add the same to the recipient
+        allowance[_from][msg.sender] -= _value;
+        Transfer(_from, _to, _value);
+        return true;
+    }
+
+    function burn(uint256 _value) returns (bool success) {
+        if (balanceOf[msg.sender] < _value) throw;            // Check if the sender has enough
+        balanceOf[msg.sender] -= _value;                      // Subtract from the sender
+        totalSupply -= _value;                                // Updates totalSupply
+        Burn(msg.sender, _value);
+        return true;
+    }
+
+    function burnFrom(address _from, uint256 _value) returns (bool success) {
+        if (balanceOf[_from] < _value) throw;                // Check if the sender has enough
+        if (_value > allowance[_from][msg.sender]) throw;    // Check allowance
+        balanceOf[_from] -= _value;                          // Subtract from the sender
+        totalSupply -= _value;                               // Updates totalSupply
+        Burn(_from, _value);
+        return true;
+    }
+}
+
+contract Coin is MyToken, Wallet, Influence {
+    
+    mapping(address => Bid) public bids;
     
     function getRate() public returns (uint) {
         
@@ -93,14 +187,14 @@ contract Coin is Wallet, Influence {
         return getWeightValue();
     }
     
-    function bidIncrease(address from, address _bidAddress, uint amountIncrease) payable {
+    function bidIncrease(address _from, address _bidAddress, uint amountIncrease) payable {
         
-        from.transfer(amountIncrease);
+        Bid bid = bids[_bidAddress];
     }
     
-    function buy(address from, address to, uint _amount) payable {
+    function buy(address _from, address _to, uint _amount) payable {
         
-        from.transfer(_amount);
+        transferFrom(_from, _to, _amount);
     }
 }
 
@@ -651,7 +745,7 @@ contract GiftCoin is Base, Coin {
     
     function orgBuy(address orgAddress, uint _amount) public {
         
-        buy(orgAddress, _amount);
+        // buy(orgAddress, _amount);
     }
     
     function orgSimpleAward(address orgAddress, bytes1[] _personnalCountryIdCardNubmers) public returns (Award award){
@@ -667,6 +761,8 @@ contract GiftCoin is Base, Coin {
         
         Eshop eshop = eshops[eshopAddress];
         bid = eshop.simpleBid(_artefactSerialNumbers, _imageURls, _dateStart, _duration, _amountMin);
+        
+        bids[bid] = bid;
         
         return bid;
     }
